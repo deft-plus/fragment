89e4bb53b9a9f9b4c58889990b80629ffc1fe51e
Miguel Bogota <miguel.bogota@mercadolibre.com.co>
2024-09-11 23:01:08 -0500

    docs(reactive): implements docs using github pages (#11)
    * Adds initial changelog for the reactive module and removes the CHANGELOG.md
    
    * Changes pr title lint to use all lower case letters
    
    * Adds everything
    
    * Fixes styles
    
    * Fixes lint issues
    
    * Finishes some documentation

---
+++ .docs/_config.ts
@@ -0,0 +1,47 @@
+import lume from 'lume/mod.ts';
+import robots from 'lume/plugins/robots.ts';
+import sass from 'lume/plugins/sass.ts';
+import terser from 'lume/plugins/terser.ts';
+import minifyHTML from 'lume/plugins/minify_html.ts';
+import lightningCss from 'lume/plugins/lightningcss.ts';
+import sitemap from 'lume/plugins/sitemap.ts';
+import multilanguage from 'lume/plugins/multilanguage.ts';
+import favicon from 'lume/plugins/favicon.ts';
+
+const site = lume({
+  cwd: new URL('.', import.meta.url).pathname,
+});
+
+site
+  .ignore('README.md')
+  .copy('assets')
+  .use(multilanguage({
+    languages: ['en'],
+    defaultLanguage: 'en',
+  }))
+  .use(favicon())
+  .use(robots())
+  .use(sitemap())
+  .use(sass({
+    format: 'compressed',
+  }))
+  .use(terser({
+    options: {
+      compress: true,
+      mangle: true,
+    },
+  }))
+  .use(minifyHTML({
+    options: {
+      keep_closing_tags: true,
+      minify_css: true,
+      minify_js: true,
+    },
+  }))
+  .use(lightningCss({
+    options: {
+      minify: true,
+    },
+  }));
+
+export default site;
---
+++ .docs/_includes/components/navigation.ts
@@ -0,0 +1,38 @@
+import { html } from '@/includes/helpers/templates.ts';
+
+const logo = html`
+  <a class="logo" href="/">
+    <img src="/favicon.svg" alt="Logo" />
+    <p>Fragment</p>
+  </a>`;
+
+const link = (href: string, content: string, blank: boolean) =>
+  html`<li class="link"><a href="${href}" ${blank ? 'target="_blank"' : ''}>${content}</a></li>`;
+
+const linksContent = [
+  {
+    href: 'https://jsr.io/@fragment',
+    content: html`<img src="/assets/jsr-logo.svg" alt="JSR logo" width="30" />`,
+    blank: true,
+  },
+  {
+    href: 'https://github.com/deft-plus/fragment',
+    content: html`<img src="/assets/github-logo.svg" alt="Github logo" width="20" />`,
+    blank: true,
+  },
+  // { href: '/changelog', content: 'Changelog', blank: false },
+];
+
+const links = html`
+  <ul class="links">
+    ${linksContent.map(({ href, content, blank }) => link(href, content, blank)).join('')}
+  </ul>`;
+
+export const navigation = html`
+  <nav class="navigation">
+    <div class="container">
+      ${logo}
+      ${links}
+    </div>
+  </nav>
+  <nav class="navigation offset"></nav>`;
